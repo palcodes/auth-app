10dd3c461b635570f46ca7bcad9ded9ed134720a
Aayush Pal <ap.aayushpal@gmail.com>
2020-05-29 16:23:37 +0530

    refactored main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:makestories_interview/ui/home.dart';
-import 'package:makestories_interview/ui/login.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:makestories_interview/ui/signup.dart';
+import 'package:makestories_interview/abstractions/auth.dart';
+import 'package:makestories_interview/abstractions/user.dart';
+import 'package:makestories_interview/session.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,27 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        textTheme: GoogleFonts.workSansTextTheme(
-          Theme.of(context).textTheme,
-        )
-      ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.onAuthStateChanged,
-        builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
-          if (snapshot.connectionState == ConnectionState.active) {
-            bool isLoggedIn = snapshot.hasData;
-            if (isLoggedIn) {
-              return HomeScreen();
-            } else {
-              return LoginScreen();
-            }
-          }
-          else {
-            return Container();
-          }
-        },
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Session(),
       ),
     );
   }
